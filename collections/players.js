6594a4101b15ad1d64b81a7796e718501052578f
jdlnolen <jdlnolen@gmail.com>
2013-08-29 17:29:28 -0400

    fixture data for blood pressure added along with better api structure

--- collections/players.js
+++
@@ -1,9 +0,0 @@
-Players = new Meteor.Collection("players");
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    collectionApi = new CollectionAPI({ authToken: '97f0ad9e24ca5e0408a269748d7fe0a0' });
-    collectionApi.addCollection(Players, 'players');
-    collectionApi.start();
-  });
-}
